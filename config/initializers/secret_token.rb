b40d13ed9892a291502468eb9e4d53a391cb812d
Folashade O <f.okunubi@gmail.com>
2012-04-06 02:52:58 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testh::Application.config.secret_token = 'c6244e3c62e1d7912fd43bda7d06ddda6c6b9c9a2b6a336959e7fe1dce55c5694efbce71370f09cc7b08c5715cd83e106924cdd811f220bec50e4e2098d41af9'
